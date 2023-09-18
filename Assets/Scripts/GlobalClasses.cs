02183526e533230c70983cefc59a46bae8d79aa4
Feifan Lai <joelai0723@gmail.com>
2023-09-18 15:24:31 +0800

    [Feature] Get create new account signatures and relative example

--- Assets/Scripts/GlobalClasses.cs
+++ Assets/Scripts/GlobalClasses.cs
@@ -16,3 +16,52 @@ public struct KeyPair
         this.private_key = priv;
     }
 }
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Signatures
+{
+    public string r_sig;
+    public string s_sig;
+
+    public Signatures(string r_sig, string s_sig)
+    {
+        this.r_sig = r_sig;
+        this.s_sig = s_sig;
+    }
+
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct SignatureInput
+{
+    public string private_key;
+    public string self_contract_address;
+    public string to;
+    public string nounce;
+    public string max_fee;
+    public string high;
+
+    public SignatureInput(string private_key, string self_contract_address, string to, string nounce, string max_fee, string high)
+    {
+        this.private_key = private_key;
+        this.self_contract_address = self_contract_address;
+        this.to = to;
+        this.nounce = nounce;
+        this.max_fee = max_fee;
+        this.high = high;
+    }
+}
+
+
+[StructLayout(LayoutKind.Sequential)]
+public struct SignatureAccountDeployInput
+{
+    public string private_key;
+    public string public_key;
+    public string nonce;
+    public string max_fee;
+    public string salt;
+
+}
+
+
--- Assets/Scripts/StarknetBridge.cs
+++ Assets/Scripts/StarknetBridge.cs
@@ -14,11 +14,32 @@ public static class StarknetBridge
     public static extern KeyPair generate_key_pair();
 
     /// <summary>
-    /// This M
+    /// This method to generate user contract address, 
+    /// for now, you should put the generated public key to generate relative contract address
     /// </summary>
     /// <param name="public_key">It must be hex, like "0xsdkhf54iu45..."</param>
-    /// <returns>Example "0x89ISd6565f..."</returns>
+    /// <returns>Bigint string</returns>
     [DllImport("starknet_bridge")]
     public static extern string get_contract_address(string public_key);
+
+    /// <summary>
+    /// This method is to get the signaturse for deploy the new account on Starknet test chain
+    /// </summary>
+    /// <param name="input">The relative fields like public key and private key must be lower case hex string, like "0xsdkhf54iu45...",
+    /// more info please check example
+    /// <returns>Signatures</returns>
+    [DllImport("starknet_bridge")]
+    public static extern Signatures get_deploy_account_signature(SignatureAccountDeployInput input);
+
+    /// <summary>
+    /// This method is to get the signatures for make transaction
+    /// </summary>
+    /// <param name="input">The relative fields like public key and private key must be lower case hex string, like "0xsdkhf54iu45...",
+    /// more info please check example
+    /// </param>
+    /// <returns>Signatures</returns>
+    [DllImport("starknet_bridge")]
+    public static extern Signatures get_general_signature(SignatureInput input, IntPtr[] strings, int count, string selector);
+
 
 }
